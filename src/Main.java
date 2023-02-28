50ac20faa235bf5b11a60b5cfb3ff1baea877152
KrisHite <alexgusevmail@gmail.com>
2023-02-28 20:26:08 +0500

    Коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = null;
+            BufferedWriter bw = null;
+            try {
+// Создание потоков для чтения и записи с нужной кодировкой
+                br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\My\\MyFile1.txt"),"cp1251"));
+                bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("C:\\My\\MyFile2.txt"),"cp1251"));
+// Переписывание информации из одного файла в другой
+                int lineCount = 0; // счетчик строк
+                String s;
+                while ((s = br.readLine()) != null) { lineCount++;
+                    System.out.println(lineCount + ": " + s);
+                    bw.write(lineCount + ": " + s); // запись без перевода строки
+                    bw.newLine(); // принудительный переход на новую строку
+                }
+            } catch (IOException e) {
+                System.out.println("Ошибка!!!!!!!!");
+            }
+            finally{
+                br.close();
+                bw.flush();
+                bw.close();
+            }
+        }
 }
